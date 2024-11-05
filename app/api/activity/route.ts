7b9fee488637f3c93d18c2f5572bb57027908ccd
Ree-m <reembsrat@gmail.com>
2024-11-06 01:44:53 +0300

    ticker changes

--- app/api/activity/route.ts
+++ app/api/activity/route.ts
@@ -19,11 +19,10 @@ export async function GET() {
         data = result.rows;
     } catch (error) {
         console.log(error)
-        return Response.json({ error: error})
+        return Response.json({ error: error }, { status: 500 })
     }
     const topViewed = data.map((company) => ({
         [company.ticker]: company.views
     }))
-    console.log("top viewed", topViewed)
     return new Response(JSON.stringify(topViewed));
 }
--- app/api/ticker/[ticker]/route.ts
+++ app/api/ticker/[ticker]/route.ts
@@ -6,6 +6,24 @@ export async function GET(req: Request, { params }: { params: { ticker: string }
     const ticker = params.ticker
     let data
 
+    try {
+        const response = await fetch("https://www.sec.gov/files/company_tickers_exchange.json", {
+            headers: {
+                "User-Agent": "Test (test@gmail.com)"
+            }
+        });
+        data = await response.json()
+
+    } catch (error) {
+        return Response.json({ error: error }, { status: 500 })
+    }
+
+    const companies: Company[] = data.data
+    const selectedCompany = companies.filter((company: Company) => company[2] == ticker)
+
+    if (selectedCompany.length === 0) {
+        return new Response(JSON.stringify({ error: "Company not found" }), { status: 404 });
+    }
     await pool.query(
         `
         INSERT INTO companies_views (ticker, views)
@@ -15,19 +33,7 @@ export async function GET(req: Request, { params }: { params: { ticker: string }
         `,
         [ticker]
     );
-    try{
-        const response = await fetch("https://www.sec.gov/files/company_tickers_exchange.json", {
-            headers: {
-                "User-Agent": "Test (test@gmail.com)"
-            }
-        });
-         data = await response.json()
 
-    }catch(error){
-        return Response.json({ error: error})
-    }
 
-    const companies:Company[]= data.data
-    const selectedCompany = companies.filter((company: Company) => company[2] == ticker)
     return Response.json(selectedCompany)
 }
